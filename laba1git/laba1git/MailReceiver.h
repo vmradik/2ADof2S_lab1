db87ddc344fd0326d055d03a1489432c824bd3ea
vmradik <vm.radik@gmail.com>
2016-12-21 17:09:21 +0300

    ветвь Ололо

---
+++ laba1git/laba1git/MailReceiver.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#include "Colleague.h"
+
+class CMailReceiver : public CColleague
+{
+public:
+	virtual void receive_mail(const std::string& message, const std::string& target_name) = 0;
+	virtual void set_name(const std::string& name) = 0;
+	virtual std::string get_name(void) = 0;
+};
+
