d00481d43ca85a6e9b747ee81fb43c64f443048d
AllaChizh <drunk-coder@yandex.ru>
2016-12-21 17:30:28 +0300

    123merge

---
+++ laba1git/laba1git/Colleague.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class CMediator;
+
+class CColleague
+{
+public:
+	virtual void add_mediator(CMediator* mediator) = 0;
+	virtual void send_message(const std::string& message, const std::string& target_name) = 0;
+};
+
---
+++ laba1git/laba1git/Mediator.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Colleague.h"
+
+class CMediator
+{
+public:
+	virtual void add_colleague(CColleague* colleague) = 0;
+	virtual void remove_colleague(CColleague* colleague) = 0;
+	virtual void process_message(const std::string& message, const std::string& target_name) = 0;
+};
+
